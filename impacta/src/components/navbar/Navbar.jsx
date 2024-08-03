5dcf98c2ca9b6037425cd7c2f1dd4befb1d98a68
ANTHONY <anthonychukwudiDev@outlook.com>
2024-08-03 04:28:43 -0300

    feat: finalizando responsividade da tela impacto

--- impacta/src/components/navbar/Navbar.jsx
+++ impacta/src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import logo from '../../assets/logoImpacta.png'
+import React from "react";
+import logo from "../../assets/logoImpacta.png";
 
 export default function () {
     return (
@@ -10,14 +10,24 @@ export default function () {
                     <li className="cursor-pointer">Home</li>
                     <li className="cursor-pointer">Quem Somos</li>
                     <li className="cursor-pointer text-corPrincipal">Quiz</li>
-                    <li className="cursor-pointer text-corPrincipal">Mapeamento</li>
+                    <li className="cursor-pointer text-corPrincipal">
+                        Mapeamento
+                    </li>
                 </ul>
                 <div className="flex justify-center items-center gap-3">
-                    <div className="w-40 h-12 flex items-center justify-center border-4 border-corSecudanria rounded-2xl cursor-pointer"><p className="text-corSecudanria font-semibold text-xl">Entrar</p></div>
+                    <div className="w-40 h-12 flex items-center justify-center border-4 border-corSecudanria rounded-2xl cursor-pointer">
+                        <p className="text-corSecudanria font-semibold text-xl">
+                            Entrar
+                        </p>
+                    </div>
                     <div className="w-1 h-12 bg-corSecudanria"></div>
-                    <div className="w-40 h-12 flex items-center justify-center bg-corSecudanria rounded-2xl cursor-pointer"><p className="text-white font-semibold text-xl">Cadastre-se</p></div>
+                    <div className="w-40 h-12 flex items-center justify-center bg-corSecudanria rounded-2xl cursor-pointer">
+                        <p className="text-white font-semibold text-xl">
+                            Cadastre-se
+                        </p>
+                    </div>
                 </div>
             </nav>
         </div>
-    )
+    );
 }
--- impacta/src/pages/home/Home.jsx
+++ impacta/src/pages/home/Home.jsx
@@ -1,31 +1,52 @@
-import React from 'react'
-import fotoHome from "../../assets/fotoHome.png"
-import fotoQueimada from "../../assets/fotoQueimada.png"
-import fotoUsina from "../../assets/fotoUsina.png"
+import React from "react";
+import fotoHome from "../../assets/fotoHome.png";
+import fotoQueimada from "../../assets/fotoQueimada.png";
+import fotoUsina from "../../assets/fotoUsina.png";
+
 export default function Home() {
     return (
-        <div className="bg-fundoPrincipal h-screen w-full">
-            <main className="gap-16 flex flex-col">
-                <div className="flex  gap-15 bg-[url('./assets/Back.png')] bg-no-repeat bg-cover  ">
-                    <div className="flex flex-col justify-end w-6/12 items-center gap-10  h-96">
-                        <div className=" flex flex-col justify-center">
-                            <h1 className="font-regular text-corPrincipal text-8xl " >IMPACTA</h1>
-                            <p className=" w-96 text-2sm text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ipsa quibusdam quidem dolores impedit modi consequuntur provident eaque, exercitationem, perferendis rerum cupiditate? Quae facere sit corrupti. Non totam nihil quos?</p>
-                        </div>
-                        <div className=" bg-corPrincipal w-72 h-10  flex justify-center rounded-[50px] items-center text-xl cursor-pointer"><p className="font-semibold text-white">Trilha da consciência</p></div>
+        <main className="gap-16 flex flex-col bg-fundoPrincipal w-full">
+            <div className="flex  gap-15 bg-[url('./assets/Back.png')] bg-no-repeat bg-cover  ">
+                <div className="flex flex-col justify-end w-6/12 items-center gap-10  h-96">
+                    <div className=" flex flex-col justify-center">
+                        <h1 className="font-regular text-corPrincipal text-8xl ">
+                            IMPACTA
+                        </h1>
+                        <p className=" w-96 text-2sm text-white">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aperiam ipsa quibusdam quidem dolores impedit
+                            modi consequuntur provident eaque, exercitationem,
+                            perferendis rerum cupiditate? Quae facere sit
+                            corrupti. Non totam nihil quos?
+                        </p>
+                    </div>
+                    <div className=" bg-corPrincipal w-72 h-10  flex justify-center rounded-[50px] items-center text-xl cursor-pointer">
+                        <p className="font-semibold text-white">
+                            Trilha da consciência
+                        </p>
                     </div>
                 </div>
-                <div className="flex justify-around">
-                    <div className="flex gap-10">
-                        <img className='h-56' src={fotoQueimada} alt="" />
-                        <img className='h-56' src={fotoUsina} alt="" />
-                    </div>
-                    <div className="flex flex-col gap-10">
-                        <p className="w-96 text-2sm text-white ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        <div className=" bg-corPrincipal w-72 h-10  flex justify-center rounded-[50px] items-center text-x cursor-pointer"><p className="font-semibold text-white">Mapeamento local</p></div>
+            </div>
+            <div className="flex justify-around mb-12">
+                <div className="flex gap-10">
+                    <img className="h-56" src={fotoQueimada} alt="" />
+                    <img className="h-56" src={fotoUsina} alt="" />
+                </div>
+                <div className="flex flex-col gap-10 ">
+                    <p className="w-96 text-2sm text-white ">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                    </p>
+                    <div className=" bg-corPrincipal w-72 h-10  flex justify-center rounded-[50px] items-center text-x cursor-pointer">
+                        <p className="font-semibold text-white">
+                            Mapeamento local
+                        </p>
                     </div>
                 </div>
-            </main>
-        </div>
-    )
+            </div>
+        </main>
+    );
 }
